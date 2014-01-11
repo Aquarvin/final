b15e680e57e4f19994784b0d33f3bc860761e151
Aquarvin <fevius@gmail.com>
2014-01-11 12:27:30 +0200

    added eauth

--- protected/controllers/NoteController.php
+++ protected/controllers/NoteController.php
@@ -31,6 +31,43 @@ class NoteController extends Controller
 	}
 	public function actionLogin()
 	{
+		$serviceName = Yii::app()->request->getQuery('service');
+        if (isset($serviceName)) {
+            /** @var $eauth EAuthServiceBase */
+            $eauth = Yii::app()->eauth->getIdentity($serviceName);
+            $eauth->redirectUrl = Yii::app()->user->returnUrl;
+            $eauth->cancelUrl = $this->createAbsoluteUrl('site/login');
+            try {
+            		//echo "sdfsdf"; die;
+                if ($eauth->authenticate()) {
+                    //var_dump($eauth->getIsAuthenticated(), $eauth->getAttributes());
+                    $identity = new EAuthUserIdentity($eauth);
+
+                    // successful authentication
+                    if ($identity->authenticate()) {
+                        Yii::app()->user->login($identity);
+                        //var_dump($identity->id, $identity->name, Yii::app()->user->id);exit;
+
+                        // special redirect with closing popup window
+                        $eauth->redirect();
+                    }
+                    else {
+                        // close popup window and redirect to cancelUrl
+                        $eauth->cancel();
+                    }
+                }
+
+                // Something went wrong, redirect to login page
+                $this->redirect(array('site/login'));
+            }
+            catch (EAuthException $e) {
+                // save authentication error to session
+                Yii::app()->user->setFlash('error', 'EAuthException: '.$e->getMessage());
+
+                // close popup window and redirect to cancelUrl
+                $eauth->redirect($eauth->getCancelUrl());
+            }
+        }
 		$model = new LoginForm;
 
 		// collect user input data
